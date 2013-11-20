54254df92865cb745e55c08f1019a5e2efcf2665
Sahil Ohri <sahilrohri@gmail.com>
2013-11-19 22:38:33 -0500

    Product and word clustering

---
+++ BestBuyRecommendation/src/com/patternminers/Product.java
@@ -0,0 +1,29 @@
+package com.pattern.miners;
+
+public class Product {
+	String name;
+	long skuID;
+	
+	public Product(){
+		this.name = null;
+		this.skuID = Long.MIN_VALUE;
+	}
+	
+	public Product(long skuID, String name){
+		this.skuID = skuID;
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getSkuID() {
+		return skuID;
+	}
+	public void setSkuID(long skuID) {
+		this.skuID = skuID;
+	}
+}
---
+++ BestBuyRecommendation/src/com/patternminers/WordCleaner.java
@@ -0,0 +1,54 @@
+package com.pattern.miners;
+
+import java.util.HashSet;
+import java.util.regex.*;
+
+public class WordCleaner {
+	CheckQuerySimilarity cs = new CheckQuerySimilarity();
+	public HashSet<String> loadProductNameWords(String productName, HashSet<String> words){
+		//HashSet<String> words = new HashSet<String>();
+		String[] wordArray = productName.split(" ");
+		for(String word : wordArray){
+			if(isNumeric(word) || word.equals(" ")){
+				continue;
+			}
+			words.add(word.trim());
+		}
+		return words;
+	}
+	
+	public String matchQueryWordToProduct(String oldQuery, HashSet<String> prodWords){
+		String[] oldQueryArray = oldQuery.split(" ");
+		//System.out.println(oldQuery);
+		StringBuilder sb = new StringBuilder();
+		for(String word : oldQueryArray){
+			if(isNumeric(word) || word.equals(" ")){
+				sb.append(word);
+			}
+			else{
+				word = correctWord(word.trim(), prodWords);
+				sb.append(word);
+			}
+			sb.append(" ");
+		}
+		//System.out.println(sb.toString());
+		return sb.toString();
+	}
+	
+	private String correctWord(String word, HashSet<String> prodWords){
+		String newWord = null;
+		float oldSimVal = Integer.MIN_VALUE;
+		for(String s : prodWords){
+			float newSimVal = cs.getDistance(s, word);
+			if(newSimVal > oldSimVal){
+				newWord = s;
+				oldSimVal = newSimVal;
+			}
+		}
+		return newWord;
+	}
+	
+	private boolean isNumeric(String word){
+		return  Pattern.matches("\\d+", word);
+	}
+}
